f8ddf636e89ee4b441ae1c7b77cc5225599a1640
Quangvn7 <quangvn7@viettel.com.vn>
2025-08-01 14:48:05 +0700

    feat: create fundraise component

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -26,7 +26,7 @@ const Header = () => {
                     <Link href={'/browse'}>
                         Browse
                     </Link>
-                    <Link href={'/'}>
+                    <Link href={'/fundraise'}>
                         Fundraise
                     </Link>
                     <hr />
@@ -58,7 +58,7 @@ const Header = () => {
                 <Link href={'/browse'}>
                     Browse
                 </Link>
-                <Link href={'/'}>
+                <Link href={'/fundraise'}>
                     Fundraise
                 </Link>
             </div>
