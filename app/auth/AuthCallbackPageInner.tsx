2e6997a68933a5718d734369baec1c676e6e63c7
Quang Vu Ngoc <vungocquang27102001@gmail.com>
2025-08-16 10:36:24 +0700

    feat: hot fix auth

--- app/auth/AuthCallbackPageInner.tsx
+++ app/auth/AuthCallbackPageInner.tsx
@@ -10,21 +10,18 @@ export default function AuthCallbackPageInner() {
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    console.log("window.location.href:", window.location.href);
-    console.log("searchParams:", searchParams.toString());
     const token = searchParams.get("token");
-    console.log("token:", token);
     async function fetchUserAndRedirect() {
       if (token) {
         Cookies.set("token", token, { path: "/", expires: 7 });
       }
-      //   try {
-      //     const user = await fetcher("/auth/get-user");
-      //     useUserStore.getState().setUser(user);
-      //     router.replace("/");
-      //   } catch {
-      //     router.replace("/login");
-      //   }
+        try {
+          const user = await fetcher("/auth/get-user");
+          useUserStore.getState().setUser(user);
+          router.replace("/");
+        } catch {
+          router.replace("/login");
+        }
     }
     fetchUserAndRedirect();
   }, [router, searchParams]);
